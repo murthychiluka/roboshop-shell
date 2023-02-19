45da72709dd15a792b2056885588ac6294af139f
Murthy <murthychiluka@gmail.com>
2023-02-19 19:45:55 +0530

    catalogue changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,20 +1,47 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app 
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-cd /app 
-unzip /tmp/catalogue.zip
-cd /app 
-npm install 
-cp configs/catalogue.service /etc/systemd/system/catalogue.service
+. common.sh
 
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl start catalogue
+print_head "Configure NodeJS Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org-shell -y
+print_head "Install NodeJS"
+yum install nodejs -y &>>${log_file}
 
-mongo --host mongodb-dev.murthychiluka.online </app/schema/catalogue.js
+print_head "Create Roboshop User"
+useradd roboshop &>>${log_file}
+
+print_head "Create Application Directory"
+mkdir /app &>>${log_file}
+
+print_head "Delete Old Content"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading App Content"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
+cd /app
+
+print_head "Extracting App Content"
+unzip /tmp/catalogue.zip &>>${log_file}
+
+print_head "Installing NodeJS Dependencies"
+npm install &>>${log_file}
+
+print_head "Copy SystemD Service File"
+cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${log_file}
+
+print_head "Enable Catalogue Service "
+systemctl enable catalogue &>>${log_file}
+
+print_head "Start Catalogue Service"
+systemctl restart catalogue &>>${log_file}
+
+print_head "Copy MongoDB Repo File"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head "Install Mongo Client"
+yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "Load Schema"
+mongo --host mongodb-dev.murthychiluka.online </app/schema/catalogue.js &>>${log_file}
