2385c5634ff83919188476a074f487be9d9d8851
Murthy <murthychiluka@gmail.com>
2023-02-20 16:18:47 +0530

    4

--- Frontend.sh
+++ Frontend.sh
@@ -4,6 +4,7 @@ source common.sh
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
 source_check $?
+
 print_head "Removing Old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 source_check $?
@@ -11,6 +12,7 @@ source_check $?
 print_head "Downloading Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 source_check $?
+
 print_head "Extracting Downloaded Frontend"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
@@ -19,9 +21,11 @@ source_check $?
 print_head "Copying Nginx Config for RoboShop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 source_check $?
+
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
 source_check $?
+
 print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}
 source_check $?
