c0921ffe7fb60037fa7bb26ca8814c2f4e4e817e
Murthy <murthychiluka@gmail.com>
2023-02-21 22:12:45 +0530

    s

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 source common.sh
 mysql_root_password=$1
-if [ -z "{mysql_root_password}"]; then
+if [ -z "{mysql_root_password}" ]; then
 echo -e "\e[31m"Missing MySQL root password argument \e[0m"
 exit1
 fi
