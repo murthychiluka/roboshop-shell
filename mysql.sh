f01e009205723e6ecd34d6c1a25d69174dc0cdc7
Murthy <murthychiluka@gmail.com>
2023-02-21 22:40:18 +0530

    c

--- mysql.sh
+++ mysql.sh
@@ -25,6 +25,9 @@ print_head "start mySQL Service"
 systemctl start mysqld   &>>${log_file}
 status_check $?
 
-print_head "set password"
-mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log_file}
+print_head "Set Root Password"
+echo show databases | mysql -uroot -p${mysql_root_password} &>>${log_file}
+if [ $? -ne 0 ]; then
+  mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log_file}
+fi
 status_check $?
