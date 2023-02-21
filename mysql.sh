eac5d17c6c4c04e8056cf443e2ed9e71276f9679
Murthy <murthychiluka@gmail.com>
2023-02-21 22:21:22 +0530

    a

--- mysql.sh
+++ mysql.sh
@@ -6,21 +6,21 @@ if [ -z "${mysql_root_password}" ]; then
   exit 1
 fi
 print_head "disabling mysql 8 version"
-dnf module disable mysql -y 
+dnf module disable mysql -y  &>>${log_file}
 status_check $?
 
 print_head "Installing MYSQL Server"
-yum install mysql-community-server -y
+yum install mysql-community-server -y  &>>${log_file}
 status_check $?
 
 print_head "enable mysql service"
-systemctl enable mysqld
+systemctl enable mysqld  &>>${log_file}
 status_check $?
 
 print_head "start mySQL Service"
-systemctl start mysqld  
+systemctl start mysqld   &>>${log_file}
 status_check $?
 
 print_head "set password"
-mysql_secure_installation --set-root-pass ${mysql_root_password}
+mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log_file}
 status_check $?
